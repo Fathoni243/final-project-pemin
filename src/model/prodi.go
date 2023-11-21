f55d7032d0e2d19ab9c7b9a6ecd341360ab06e1c
Fathoni243 <ahmadfathoni503@gmail.com>
2023-11-22 00:32:33 +0700

    remove response json mahasiswa

--- src/model/prodi.go
+++ src/model/prodi.go
@@ -4,6 +4,6 @@ type (
 	Prodi struct {
 		ID        int64       `json:"id" gorm:"primaryKey"`
 		Nama      string      `json:"nama" gorm:"not null;type:varchar(100)"`
-		Mahasiswa []Mahasiswa `json:"mahasiswa" gorm:"foreignKey:ProdiId"`
+		Mahasiswa []Mahasiswa `json:"-" gorm:"foreignKey:ProdiId"`
 	}
 )
